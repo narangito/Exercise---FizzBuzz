709bb4906db3c09a6cbaf45979b9d591c63f75f3
narangito <rolfs24@gmail.com>
2016-06-12 23:46:26 +0200

    "Second part exercise FizzBuzz"

---
+++ FizzBuzz2.rb
@@ -0,0 +1,12 @@
+(1..100).each do |i|
+	if (i % 3 == 0) && (i % 5 == 0)
+		result = "FizzBuzz"
+	elsif (i % 3 == 0)
+		result = "Fizz"
+	elsif (i % 5 == 0)
+		result = "Buzz"
+	else
+		result = i
+	end
+	puts result
+end
